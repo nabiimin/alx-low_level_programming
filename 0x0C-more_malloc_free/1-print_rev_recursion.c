bdd150f558d007760d6ac11c9c470b5e5293f5e3
nabiimin <yasmine.business24@gmail.com>
2023-09-08 16:07:55 -0700

    1

--- 0x0C-more_malloc_free/1-print_rev_recursion.c
+++ 0x0C-more_malloc_free/1-print_rev_recursion.c
@@ -1,13 +1,41 @@
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * _print_rev_recursion - Main Entry
- * @s: input
+ * string_nconcat - Main Entry
+ * @s1: input
+ * @s2: input
+ * @n: input
+ * Return: 0
  */
-void _print_rev_recursion(char *s)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	if (*s != '\0')
+	unsigned int i, j, k;
+	char *s;
+
+	if (s1 == NULL)
+		i = 0;
+	else
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		for (i = 0; s1[i]; i++)
+			;
 	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	if (j > n)
+		j = n;
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
+	return (s);
 }
