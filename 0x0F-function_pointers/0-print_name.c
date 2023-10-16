5d803b50d10eaecc6337eaf463400a6834395bf6
nabiimin <yasmine.business24@gmail.com>
2023-10-16 07:42:45 -0700

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,20 +1,13 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name as is
- * @name: name of the person
- * @f: is pointer
-**/
-
+ * print_name - function that prints a name.
+ * @name: string to be printed
+ * @f: pointer to function that take in a pointer to char
+ *
+ * Return: void(nothing)
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
-		return;
-	}
-	else
-	{
-		f(name);
-	}
-} 
+	f(name);
+}
